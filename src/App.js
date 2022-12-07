b3aaa27bcbeddd8884df4649bc5246d82767ebfb
Rudie G <rauoldm@gmail.com>
2022-12-07 09:18:30 -0600

    new prototype fully functional but for real

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Header from './components/Header';
 import About from './components/About';
-import Projects from './components/Projects';
 import Navigation from './components/Navigation';
 
 
